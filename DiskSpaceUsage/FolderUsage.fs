cf079b69fa60fbef045fe5cb6a5c643137088186
Damien Le Berrigaud <git@itsdamo.com>
2021-03-21 17:24:13 -0600

    Keep track of files read.

--- DiskSpaceUsage/FolderUsage.fs
+++ DiskSpaceUsage/FolderUsage.fs
@@ -1,19 +1,36 @@
 ﻿module DiskSpaceUsage.FolderUsage
 
+open System.IO
 open FolderPath
 
-type FolderSize =
+type SizeOnDisk =
     Bytes of int64
+
+type FilePath =
+    private FilePath of string
+
+type FileUsage =
+    private { path: FilePath
+              size: SizeOnDisk }
 
 type FolderUsage =
     private { path: FolderPath
-              size: FolderSize
-              children: FolderUsage list }
+              size: SizeOnDisk
+              subFiles: FileUsage list
+              subFolders: FolderUsage list }
+
+[<RequireQualifiedAccess>]
+module FileUsage =
+    let create (fileInfo: FileInfo) =
+        { path = FilePath fileInfo.FullName
+          size = Bytes fileInfo.Length }
+
+    let sizeInBytes (file: FileUsage) =
+        match file.size with
+        | Bytes bytes -> bytes
 
 [<RequireQualifiedAccess>]
 module FolderUsage =
-    open System.IO
-
     let private sizeInBytes (folder: FolderUsage) =
         match folder.size with
         | Bytes bytes -> bytes
@@ -22,29 +39,35 @@ module FolderUsage =
         async {
             let dir = DirectoryInfo (FolderPath.path folderPath)
 
-            let filesSize =
+            let subFiles =
                 dir.GetFiles()
-                |> Array.map (fun f -> f.Length)
-                |> Array.sum
+                |> Array.toList
+                |> List.map FileUsage.create
 
-            let mutable children: FolderUsage list = []
+            let mutable subFolders: FolderUsage list = []
 
             let subFolderPaths =
                 dir.GetDirectories()
                 |> Array.choose (fun d -> FolderPath.create d.FullName)
 
             for subFolderPath in subFolderPaths do
-                let! subFolderUsage = loadAsync subFolderPath
-                children <- subFolderUsage :: children
+                let! subFolder = loadAsync subFolderPath
+                subFolders <- subFolder :: subFolders
 
-            let childrenSize =
-                children
+            let filesSize =
+                subFiles
+                |> List.map FileUsage.sizeInBytes
+                |> List.sum
+
+            let foldersSize =
+                subFolders
                 |> List.map sizeInBytes
                 |> List.sum
 
             return { path = folderPath
-                     size = Bytes (filesSize + childrenSize)
-                     children = children }
+                     size = Bytes (filesSize + foldersSize)
+                     subFiles = subFiles
+                     subFolders = subFolders }
         }
 
     let size usage = usage.size
