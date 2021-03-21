8237e34c3203a210e1d7724f231a0445b2eeb56c
Damien Le Berrigaud <git@itsdamo.com>
2021-03-21 09:55:32 -0600

    Recursively calculate disk usage of a folder and all children.

---
+++ DiskSpaceUsage/FolderUsage.fs
@@ -0,0 +1,53 @@
+﻿module DiskSpaceUsage.FolderUsage
+
+type FolderPath =
+    FolderPath of string
+
+type FolderSize =
+    | Bytes of int64
+
+type FolderUsage =
+    private { path: FolderPath
+              size: FolderSize
+              children: FolderUsage list }
+
+[<RequireQualifiedAccess>]
+module FolderPath =
+    let value (FolderPath p) = p
+
+[<RequireQualifiedAccess>]
+module FolderUsage =
+
+    open System.IO
+
+    let private sizeInBytes (folder: FolderUsage) =
+        match folder.size with
+        | Bytes bytes -> bytes
+
+    let rec loadAsync (FolderPath path): Async<FolderUsage> =
+        async {
+            let dir = DirectoryInfo path
+
+            let filesSize =
+                dir.GetFiles()
+                |> Array.map (fun f -> f.Length)
+                |> Array.sum
+
+            let mutable children: FolderUsage list = []
+
+            for d in dir.GetDirectories() do
+                let subPath = FolderPath d.FullName
+                let! subUsage = loadAsync subPath
+                children <- subUsage :: children
+
+            let childrenSize =
+                children
+                |> List.map sizeInBytes
+                |> List.sum
+
+            return { path = FolderPath path
+                     size = Bytes (filesSize + childrenSize)
+                     children = children }
+        }
+
+    let size usage = usage.size
--- DiskSpaceUsage/MainUI.fs
+++ DiskSpaceUsage/MainUI.fs
@@ -5,51 +5,121 @@ open Avalonia.Controls
 open Avalonia.FuncUI.DSL
 open Avalonia.Layout
 
+open FolderUsage
+
+type AsyncData<'a> =
+    | NotLoaded
+    | Loading
+    | Loaded of 'a
+
 type Model =
     { window: Window
-      folder : string option }
+      path : FolderPath option
+      usage: AsyncData<FolderUsage> }
 
 let init window _ =
     { window = window
-      folder = None }, Cmd.none
+      path = None
+      usage = NotLoaded }, Cmd.none
 
 type Msg =
     | OpenFolderSelectDialog
-    | SelectFolder of path:string
+    | SelectFolder of FolderPath
+    | FinishLoading of FolderUsage
 
-let private selectDirectoryAsync (window: Window) =
+let private selectFolderAsync window =
     async {
         let dialog = OpenFolderDialog ()
         dialog.Title <- "Select Folder"
 
         let! folderPath = dialog.ShowAsync(window) |> Async.AwaitTask
-        return SelectFolder folderPath
+        return SelectFolder (FolderPath folderPath)
     }
+
+let private loadFolderUsageAsync path =
+    async {
+        let! usage = FolderUsage.loadAsync path
+        return FinishLoading usage
+    }
+
+let private asyncCmd = Cmd.OfAsync.result
 
 let update (msg: Msg) (model: Model) : Model * Cmd<Msg> =
     match msg with
     | OpenFolderSelectDialog ->
-        model, Cmd.OfAsync.result (selectDirectoryAsync model.window)
+        model, asyncCmd (selectFolderAsync model.window)
     | SelectFolder path ->
-        { model with folder = Some path }, Cmd.none
+        { model with path = Some path; usage = Loading }, asyncCmd (loadFolderUsageAsync path)
+    | FinishLoading usage ->
+        { model with usage = Loaded usage }, Cmd.none
+
+module FolderSizeView =
+    let private tryUnit bytesPerUnit bytes =
+        let floatBytes = float bytes
+
+        if floatBytes > bytesPerUnit
+        then Some (floatBytes / bytesPerUnit)
+        else None
+
+    let private (|Gigabytes|_|) (Bytes bytes) =
+        tryUnit (1024.0 ** 3.0) bytes
+
+    let private (|Megabytes|_|) (Bytes bytes) =
+        tryUnit (1024.0 ** 2.0) bytes
+
+    let private (|Kilobytes|_|) (Bytes bytes) =
+        tryUnit 1024.0 bytes
+
+    let display size =
+        match size with
+        | Gigabytes value -> $"%.1f{value} GB"
+        | Megabytes value -> $"%.1f{value} MB"
+        | Kilobytes value -> $"%.1f{value} KB"
+        | Bytes value -> $"%d{value} B"
 
 let view (model: Model) (dispatch: Dispatch<Msg>) =
-    let folderText = model.folder
-                     |> Option.defaultValue "No folder selected"
+    let folderText =
+        model.path
+        |> Option.map FolderPath.value
+        |> Option.defaultValue "No folder selected"
 
-    DockPanel.create [
-        DockPanel.children [
-            Button.create [
-                Button.dock Dock.Bottom
-                Button.onClick (fun _ -> dispatch OpenFolderSelectDialog)
-                Button.content "Select Directory"
-            ]
+    let usageText =
+        match model.usage with
+        | NotLoaded -> ""
+        | Loading -> "Calculating size..."
+        | Loaded usage ->
+            usage
+            |> FolderUsage.size
+            |> FolderSizeView.display
+
+    Grid.create [
+        Grid.columnDefinitions "*"
+        Grid.rowDefinitions "*, *, 50"
+        Grid.children [
             TextBlock.create [
-                TextBlock.dock Dock.Top
-                TextBlock.fontSize 48.0
-                TextBlock.verticalAlignment VerticalAlignment.Center
+                Grid.row 0
+
+                TextBlock.fontSize 24.0
+                TextBlock.verticalAlignment VerticalAlignment.Bottom
                 TextBlock.horizontalAlignment HorizontalAlignment.Center
                 TextBlock.text folderText
+            ]
+            TextBlock.create [
+                Grid.row 1
+
+                TextBlock.dock Dock.Top
+                TextBlock.fontSize 48.0
+                TextBlock.verticalAlignment VerticalAlignment.Top
+                TextBlock.horizontalAlignment HorizontalAlignment.Center
+                TextBlock.text usageText
+            ]
+            Button.create [
+                Grid.row 2
+
+                Button.dock Dock.Bottom
+                Button.height 50.0
+                Button.onClick (fun _ -> dispatch OpenFolderSelectDialog)
+                Button.content "Select Folder"
             ]
         ]
     ]
